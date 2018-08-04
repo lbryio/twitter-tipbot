b028aaf45fde6757f63ae4908cf56bb16cee483c
filipnyquist <filip@lbry.io>
2018-08-04 11:35:07 +0200

    Replaced newline chars with spaces, this will parse the messages correctly even if newlines are in the tweet, fixes #10.

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ logger.info("Started LBRY twitter tipbot.");
 stream.on("tweet", function(tweet) {
   if(tweet.user.screen_name === config.get("bot.handle").substring(1)) return;
   let msg = checkTrunc(tweet);
-  msg = msg.slice(msg.lastIndexOf(config.get("bot.handle"))).split(" ");
+  msg = msg.replace(/[\n\\]/g, " ").slice(msg.lastIndexOf(config.get("bot.handle"))).split(" ");
   if (msg.length >= 2) checkTweet(tweet, msg);
 });
 
